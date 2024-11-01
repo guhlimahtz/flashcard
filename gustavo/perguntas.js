a997a99889651380f3d4abf03c05f5dab00312c4
guhlimahtz <160292667+guhlimahtz@users.noreply.github.com>
2024-10-31 22:39:49 -0300

    Add files via upload

---
+++ gustavo/perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'História',
+    'Quando foi a segunda guerra mundial?',
+    'Aconteceu entre 1939-1945'
+)
+
+criaCartao(
+    'História',
+    'Quando foi a guerra fria?',
+    'Ocorreu entre 1947-1991'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Getulio Vargas?',
+    'Ex presidente do Brasil'
+)
+
+criaCartao(
+    'História',
+    'Quem foi Getulio Vargas?',
+    'Ex presidente do Brasil'
+)
+    
+s
